8d394ed88e824a0875b6b40ce86c0795ec1da7c3
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-16 08:37:04 -0700

    reviewing items with stars, hovering clicking

---
+++ src/Stars/review/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import StarIcon from "@mui/icons-material/Star";
+const App = () => {
+  const [star, setStar] = useState([...Array(5).keys()]);
+  const [clickedIndex, setClickedIndex] = useState(-1);
+  const [flag, setFlag] = useState(false);
+  const handleClick = (index) => {
+    setClickedIndex(index);
+    setFlag(true);
+  };
+  const handleMouseOver = (index) => {
+    setClickedIndex(index);
+  };
+  const handleMouseLeave = (index) => {
+    if (!flag) setClickedIndex(-1);
+  };
+  return (
+    <div className="App" style={{ textAlign: "center", margin: "2rem 1rem" }}>
+      {star.map((i, index) => {
+        return (
+          <StarIcon
+            key={index}
+            color={index <= clickedIndex ? "warning" : ""}
+            onClick={() => handleClick(index)}
+            onMouseOver={() => handleMouseOver(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;

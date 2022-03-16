fbf18efd4dcf78ec18c08b01f03bdfbd0e1aeeb4
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-15 18:38:06 -0700

    folder-files-print-recursion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import TicTac from "./TicTacToe/TicTac";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TicTac />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );

b9f97ce784d3744437e9ca051dddfc023e529eca
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-17 16:44:23 -0700

    added handle click and usestates

--- src/TicTacToe/App.jsx
+++ src/TicTacToe/App.jsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import "./tic.css";
 const App = () => {
   const [cells, setCells] = useState([...Array(9).keys()]);
-  console.log(cells);
+  const [xturn, setXturn] = useState(true);
+  //   console.log(cells);
+  const handleClick = (index) => {
+    let newCells = [...cells];
+    xturn ? (newCells[index] = "X") : (newCells[index] = "O");
+    setCells(newCells);
+    setXturn(!xturn);
+  };
   return (
     <div className="container">
       {cells.map((item, index) => {
         return (
-          <div className="cell" key={index}>
+          <div className="cell" key={index} onClick={() => handleClick(index)}>
             {item}
           </div>
         );

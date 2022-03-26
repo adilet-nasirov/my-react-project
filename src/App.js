7cc5263905a8b20239caf6c3d8feb3c0032e728b
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-03-26 11:02:52 -0700

    context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Context } from "./GeneralContext";
+import Details from "./RecipeSearch/Details";
+import Home from "./RecipeSearch/Home";
+import NotFoundPage from "./RecipeSearch/NotFoundPage";
 
 const App = () => {
-  return <div>App</div>;
+  const data = useContext(Context);
+  console.log(data);
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:foodLabel" element={<Details />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
